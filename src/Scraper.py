52646c801792be6a7a2733a1bb60e550e7660e94
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2020-07-21 19:22:09 -0400

    change awkward field access

--- src/Scraper.py
+++ src/Scraper.py
@@ -5,7 +5,7 @@ from QueryModel import QueryModel
 class Scraper:
     def __init__(self, driver_path, model_src):
         self.driver_path = driver_path
-        self.query_model = QueryModel(model_src)
+        self.query_model = QueryModel(model_src).model
     
     def scrape(self):
         options = Options()
@@ -13,7 +13,7 @@ class Scraper:
         driver = webdriver.Chrome(options=options, executable_path=self.driver_path)
         
         # Iterate though provided queries, and perform a scrape based on the query
-        for query in self.query_model.model["queries"]:
+        for query in self.query_model["queries"]:
             driver.get(query["source"])
             print(driver.page_source)
             driver.quit()
